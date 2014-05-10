edfdcb543df43f68d3c89de2c5ab6d39d8d9b632
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2014-05-10 14:36:48 -0300

    Adjustment in instalation

--- install.sh
+++ install.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/zsh
 #removing some files and folders
-[ -d ~/.vim ] && rm -rf ~/.vim
-[ -d ~/.i3 ] && rm -rf ~/.i3
-[ -f ~/.zshrc ] && rm -f ~/.zshrc
-[ -f ~/.vimrc ] && rm -f ~/.vimrc
+[ -d ~/.vim/autoload ] || mkdir -p ~/.vim/autoload
+[ -d ~/.vim ] || mkdir  ~/.vim
+[ -d ~/.i3 ] || mkdir -rf ~/.i3
 
-mkdir ~/.vim
-mkdir ~/.vim/autoload
-mkdir ~/.i3
-mkdir -p ~/.vim/autoload
-wget wget https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+wget https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 mv pathogen.vim ~/.vim/autoload
 mkdir bundle
 cd bundle
@@ -25,19 +20,12 @@ git clone https://github.com/honza/vim-snippets.git
 
 cd ..
 mv bundle ~/.vim/
-#mv apprentice ~/.vim/bundle/
-#mv nerdtree ~/.vim/bundle/
-#mv nercommenter ~/.vim/bundle/
-#mv tlib_vim ~/.vim/bundle/
-#mv vim-addon-mw-utils ~/.vim/bundle/
-#mv vim-snipmate ~/.vim/bundle/
-#mv vim-snippets ~/.vim/bundle/
 
-ln -s $(pwd)/vimrc ~/.vimrc
-ln -s $(pwd)/zshrc ~/.zshrc
+ln -sf $(pwd)/vimrc ~/.vimrc
+ln -sf $(pwd)/zshrc ~/.zshrc
 
 #my i3 conf files
 chmod +x i3/conky
-ln -s $(pwd)/i3/config ~/.i3/config
-ln -s $(pwd)/i3/conky ~/.i3/conky
-ln -s $(pwd)/i3/conkyrc ~/.i3/conkyrc
+ln -sf $(pwd)/i3/config ~/.i3/config
+ln -sf $(pwd)/i3/conky ~/.i3/conky
+ln -sf $(pwd)/i3/conkyrc ~/.i3/conkyrc
