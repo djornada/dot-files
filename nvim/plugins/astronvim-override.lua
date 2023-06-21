4d3a5fca8b6dc033646b9f317dea2b802fc79384
djornada <djornada@outlook.com>
2023-06-21 16:25:48 -0300

    🔧

---
+++ nvim/plugins/astronvim-override.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "goolord/alpha-nvim",
+    opts = function(_, opts)
+      opts.section.header.val = {}
+      return opts
+    end,
+  }
+}
---
+++ nvim/plugins/colors.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    config = function()
+      require("catppuccin").setup {}
+    end,
+  },
+}
