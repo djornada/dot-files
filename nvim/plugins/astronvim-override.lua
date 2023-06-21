5272fe42fe1910d2b514b663d052a1076bf5cb0d
djornada <djornada@outlook.com>
2023-06-21 16:30:58 -0300

    🔧

--- nvim/plugins/astronvim-override.lua
+++ nvim/plugins/astronvim-override.lua
@@ -5,5 +5,14 @@ return {
       opts.section.header.val = {}
       return opts
     end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = function(_, opts)
+      opts.file_ignore_patterns= {
+        "node_modules"
+      }
+      return opts
+    end,
   }
 }
