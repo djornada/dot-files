465da17b34a1fe74adb93ade0a830352493d5a05
djornada <djornada@outlook.com>
2024-12-15 21:52:30 -0300

    update mappings

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -13,8 +13,16 @@ map("n", "<leader>q", "<CMD>q<CR>")
 map("i", "jk", "<ESC>")
 map("i", "jj", "<ESC>")
 
+map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+map("n", "<leader>bD", "<cmd>:bd<cr>", { desc = "Delete Buffer and Window" })
+
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
 map("n", "<leader>e", "<CMD>Neotree toggle<CR>", {desc = "Neotree toggle"})
 map("n", "<leader>o", "<CMD>Neotree focus<CR>", {desc = "Neotree focus"})
+
 
 map('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 map('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
